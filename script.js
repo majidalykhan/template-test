bcfc38b96838f3a7e24a27816b52008e9cd81874
Majid Ali Khan <51267704+majidalykhan@users.noreply.github.com>
2021-05-27 18:33:09 +0500

    Loading spinner

--- script.js
+++ script.js
@@ -7,12 +7,27 @@ window.onload = function() {
     const authorText = document.getElementById("author");
     const twitterBtn = document.getElementById('twitter');
     const newQuoteBtn = document.getElementById('new-quote');
+    const loader = document.getElementById('loader');
+
+// Show Loader
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loader
+function complete(){
+    if(!loader.hidden){
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 
 
 
-    // Get Quote from API
+// Get Quote from API
 async function getQuote(){
-
+    loading();
 
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 
@@ -33,10 +48,11 @@ async function getQuote(){
         } else {
             quoteText.classList.remove('long-quote');
         }
-
         
         quoteText.innerText = data.quoteText;
-        console.log(data.quoteAuthor);
+        
+        // Stop Loader, Show Quote
+        complete();
 
     } catch (error) {
         getQuote();
